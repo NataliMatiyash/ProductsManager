3ab81940d3a3d5a5e69f3930747df0dc5e53093f
NataliMatiyash <n.matiyash0403@gmail.com>
2020-11-01 13:49:44 +0200

    Created empty progect

---
+++ ProductsManager/Program.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Concrete;
+using DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductsManager
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+
+        }
+
+
+
+    }
+}
